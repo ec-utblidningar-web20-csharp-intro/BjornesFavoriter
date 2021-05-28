c797da8f1cf7454d0c5f6f8fe2efce618e8df14b
Calphonar <Richardzejak@hotmail.com>
2021-05-28 11:00:20 +0200

    Me and chris create filters for query and create tables

--- database/dbFetchPoliceToDB.js
+++ database/dbFetchPoliceToDB.js
@@ -21,11 +21,17 @@ async function fetchCrimes(){
         console.log('Connected!');
 
             for (let i = 0; i < data.length; i++) {
-                var sql = `INSERT INTO crimes(type, date, place, description) VALUES ('${data[i].type}', '${data[i].datetime}', '${data[i].location.name}', '${data[i].summary}')`;
-                con.query(sql, function (err, result){
-                    if (err) throw err;
-                    console.log('Records inserted');
-            });
+
+                con.query(`SELECT * FROM crimes WHERE description = '${data[i].summary}' AND date = '${data[i].datetime}' AND type = '${data[i].type}' AND place = '${data[i].location.name}' LIMIT 1;`, function (err, result) {
+                    console.log(result);
+                    if(result == false)
+                    {
+                        var sql = `INSERT INTO crimes(type, date, place, description) VALUES ('${data[i].type}', '${data[i].datetime}', '${data[i].location.name}', '${data[i].summary}')`;
+                        con.query(sql, function (err, result){
+                            if (err) throw err;
+                        });
+                    }
+                });
         }
     });
 }
