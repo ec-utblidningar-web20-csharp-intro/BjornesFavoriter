1af8cbfda4c9754982632ab14ab013efa2d2ddc9
Jonatan Jonsson <jonatan.jonsson@utb.ecutbildning.se>
2021-06-02 13:00:20 +0200

    Lade till contact sida.

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,8 +2,11 @@ const express = require('express'),
 	router = express.Router(),
 	mainPage = require('../controllers/MainPage');
 	resultPage = require('../controllers/ResultPage');
+	contactPage = require('../controllers/ContactPage');
 
 router.get('/', mainPage.mainPage);
+
+router.get('/contactpage', contactPage.contactPage);
 
 router.get('/resultpage', resultPage.resultPageGet);
 
